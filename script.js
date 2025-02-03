4be084c6ab4d4e39bf3f3e116f47ff4abb03f8fc
SOTOBE kodjo samuel <sotobesamuel00996633@gmail.com>
2025-02-03 13:02:23 +0000

    Premier commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Script JS pour le formulaire
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+  e.preventDefault(); // Empêche le rechargement de la page
+  alert("Merci pour votre message ! Je vous répondrai bientôt.");
+});
